fce3b3f9532a50f8e6ff6474bc936d5f90443140
Kofi Quarshie <152921119+KofiQuarshie@users.noreply.github.com>
2026-07-31 14:51:15 +0100

    Delete scripts/models/train_lraspp_with_mobious.bash

--- scripts/models/train_lraspp_with_mobious.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#set -Eeuxo pipefail
-
-SCRIPT_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd "$SCRIPT_PATH/../../"
- #source .venv/bin/activate #To activate the virtual environment
- source .venv/Scripts/activate #To activate the virtual environment
-
-python src/ready/apis/train_lraspp_with_mobious.py -c configs/models/unet/config_train_lraspp_with_mobious.yaml
