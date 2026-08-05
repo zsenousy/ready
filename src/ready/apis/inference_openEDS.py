cbcd91d30ffcea193cf38cf68219ade2c6b5486e
Kofi Quarshie <kofiquarsh@gmail.com>
2026-08-05 16:06:09 +0100

    saving 5 test images from inference mobious, removing onnx, soft_argmax and prediction argmax so it only shows original image, mask and model prediction

--- src/ready/apis/inference_openEDS.py
+++ src/ready/apis/inference_openEDS.py
@@ -310,9 +310,8 @@ if __name__ == "__main__":
             ax[6, 3].set_title("ort_outs[:,:,2]")
             ax[6, 4].set_title("ort_outs[:,:,3]")
 
-
-            plt.savefig(f"{data_path} / result{j}.png")
-            plt.close()
-            break
+            if j <= 5:
+                plt.savefig(f"{data_path}/result_{j}.png")
+                plt.close()
 
        
--- src/ready/apis/inference_rti_eyes.py
+++ src/ready/apis/inference_rti_eyes.py
@@ -334,9 +334,8 @@ if __name__ == "__main__":
                 ax[6, 2].set_title("ort_outs[:,:,1]")
                 ax[6, 3].set_title("ort_outs[:,:,2]")
                 ax[6, 4].set_title("ort_outs[:,:,3]")
-
-            plt.savefig(f" {data_path} / result_{j}.png")
-            plt.close()
-            break
+            if j <= 5:
+                plt.savefig(f"{data_path}/result_{j}.png")
+                plt.close()
 
     
