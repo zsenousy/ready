39680d57672b7ba6f7424fb05495967d07375776
Kofi Quarshie <kofiquarsh@gmail.com>
2026-08-14 16:32:56 +0100

    increased epochs and rounds

--- src/ready/apis/train_federated.py
+++ src/ready/apis/train_federated.py
@@ -109,7 +109,7 @@ if __name__ == "__main__":
     #weights_path = "downloads/ready/datasets/ready/federated"
     weights = pathlib.Path(weights_path)
     
-    federated(3, weights)
+    federated(10, weights)
     logger.info(f"##################   DONE   #############")
 
     logger.info(f"############ INFERENCE FEDERATED ############")
